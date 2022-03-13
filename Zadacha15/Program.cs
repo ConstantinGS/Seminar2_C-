8f22d9b46dbdd3208b5ad3005011a0415f963aaa
Constantin <grishanoff.konstantin@yandex.ru>
2022-03-13 13:55:58 +0500

    Ex 15 compl

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите день недели: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            while(number<1 || number>7)
+            {
+                Console.WriteLine("Пожалуйста, введите день недели: ");
+                number = Convert.ToInt32(Console.ReadLine()); 
+            }
+
+            if (number == 6 || number == 7 )
+            {
+               Console.WriteLine("Выходной"); 
+            }
+            else if (number > 0 && number < 6 )
+            {
+               Console.WriteLine("Рабочий"); 
+            }
+            
+
+
+
+
+
+        }
+    }
+}   
+
+
+
+
+
+
+
+
+
