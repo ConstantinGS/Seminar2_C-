662d3709d0d628cec4219ff1a9a544f7fceb051c
Constantin <grishanoff.konstantin@yandex.ru>
2022-03-13 14:57:37 +0500

    Ex d2 compl

---
+++ Dop zadacha 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите год: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите месяц: ");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите день: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            sravnenie (year,month,day);
+
+            void sravnenie(int y, int m, int d)
+            {
+                int d_y = 2022 - y;
+                if (m==2)
+                {
+                    if (d>1)
+                    {
+                        Console.WriteLine(d_y-1);
+                    }
+                    else
+                    {
+                        Console.WriteLine(d_y);
+                    }
+                }
+                else if (m==1)
+                {
+                    Console.WriteLine(d_y);
+                }
+                else
+                {
+                    Console.WriteLine(d_y-1);
+                } 
+            }
+
+
+
+
+        }
+    }
+}   
