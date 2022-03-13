0402a5b20081e4021184e71905d9a11aa675c596
Constantin <grishanoff.konstantin@yandex.ru>
2022-03-13 13:39:02 +0500

    Ex 13 compl

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Programm
+    {
+        static 
+        void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Vvedite chislo:");
+            string number_string = Convert.ToString(Console.ReadLine());
+            
+            bool a = int.TryParse(number_string, out int num);
+            if (a)
+            {
+                if (number_string.Length < 3)
+                {
+                    Console.WriteLine("Третьего числа нет");
+
+                }
+                else
+                {
+                    char[] b = {number_string[2]};
+                    Console.WriteLine(b);
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не число ");
+                 
+            }
+
+
+
+           
+
+        }
+
+    }
+}   
+
+
+
+
+
+
+
+
+
