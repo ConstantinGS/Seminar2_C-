fcd7fceeacc6189e358fa8ca26d9512cfde7951f
Constantin <grishanoff.konstantin@yandex.ru>
2022-03-13 10:45:08 +0500

    Ex 10 compl

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Programm
+    {
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Vvedite chislo:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string number_string = Convert.ToString(number);
+            while (number_string.Length !=3)
+            {
+               Console.WriteLine("Вы ввели не трехзначное число. Попробуйте еще раз.");
+               number = Convert.ToInt32(Console.ReadLine());
+               number_string = Convert.ToString(number);
+
+            }
+            char[] a = {number_string[2], number_string[1], number_string[0]};
+            Console.WriteLine(a);
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
