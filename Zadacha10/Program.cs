9fba111e3fef118209f5a45e5f1defc00c3308cc
Constantin <grishanoff.konstantin@yandex.ru>
2022-03-16 10:54:49 +0500

    Add fun 10

--- Zadacha10/Program.cs
+++ Zadacha10/Program.cs
@@ -14,12 +14,12 @@ namespace ConsoleApp
 
             void Zerkalo(int int_number)
             {
-                string number_string = Convert.ToString(number);
+                string number_string = Convert.ToString(int_number);
                 while (number_string.Length !=3)
                 {
                     Console.WriteLine("Вы ввели не трехзначное число. Попробуйте еще раз.");
-                    number = Convert.ToInt32(Console.ReadLine());
-                    number_string = Convert.ToString(number);
+                    int_number = Convert.ToInt32(Console.ReadLine());
+                    number_string = Convert.ToString(int_number);
 
                 }
                 char[] a = {number_string[2], number_string[1], number_string[0]};
