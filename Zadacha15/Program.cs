f1b0c030b554edfd0c5af89c97e1de519f1381a1
Constantin <grishanoff.konstantin@yandex.ru>
2022-03-16 08:39:00 +0500

    Add fun 15

--- Zadacha15/Program.cs
+++ Zadacha15/Program.cs
@@ -7,21 +7,26 @@ namespace ConsoleApp
         {
             Console.WriteLine("Введите день недели: ");
             int number = Convert.ToInt32(Console.ReadLine());
-            while(number<1 || number>7)
-            {
-                Console.WriteLine("Пожалуйста, введите день недели: ");
-                number = Convert.ToInt32(Console.ReadLine()); 
-            }
+            Number_week(number);
 
-            if (number == 6 || number == 7 )
-            {
-               Console.WriteLine("Выходной"); 
+
+            void Number_week(int n_w)
+            { 
+                while(number<1 || number>7)
+                {
+                    Console.WriteLine("Пожалуйста, введите день недели: ");
+                    number = Convert.ToInt32(Console.ReadLine()); 
+                }
+
+                if (number == 6 || number == 7 )
+                {
+                Console.WriteLine("Выходной"); 
+                }
+                else if (number > 0 && number < 6 )
+                {
+                Console.WriteLine("Рабочий"); 
+                }
             }
-            else if (number > 0 && number < 6 )
-            {
-               Console.WriteLine("Рабочий"); 
-            }
-            
 
 
 
