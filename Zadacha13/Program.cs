225b4145f8ce1f87b55bf3e2d56d1f59d25d2e54
Constantin <grishanoff.konstantin@yandex.ru>
2022-03-16 08:36:00 +0500

    Add fun 13

--- Zadacha13/Program.cs
+++ Zadacha13/Program.cs
@@ -10,28 +10,31 @@ namespace ConsoleApp
 
             Console.WriteLine("Vvedite chislo:");
             string number_string = Convert.ToString(Console.ReadLine());
-            
-            bool a = int.TryParse(number_string, out int num);
-            if (a)
-            {
-                if (number_string.Length < 3)
-                {
-                    Console.WriteLine("Третьего числа нет");
+            num_3(number_string);
 
+            void num_3(string number)
+            {
+                bool a = int.TryParse(number_string, out int num);
+                if (a)
+                {
+                    if (number_string.Length < 3)
+                    {
+                        Console.WriteLine("Третьего числа нет");
+
+                    }
+                    else
+                    {
+                        char[] b = {number_string[2]};
+                        Console.WriteLine(b);
+                    }
+                
                 }
                 else
                 {
-                    char[] b = {number_string[2]};
-                    Console.WriteLine(b);
-                }
-                
-            }
-            else
-            {
-                Console.WriteLine("Вы ввели не число ");
+                    Console.WriteLine("Вы ввели не число ");
                  
+                }
             }
-
 
 
            
